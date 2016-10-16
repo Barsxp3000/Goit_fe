d633f7f1a660225463497eb41a883b3f02a9570a
Logunov Vladimir <logunov@live.ruprocon-ten git config --global user.email logunov@live.ru>
2016-10-16 01:13:34 -0700

    Reload HW 7-8

--- JS/Lesson7_8/js/script_form.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-         $('[title]').addClass('tooltip');
-            $('.tooltip').each(function() {
-               $(this).attr('data-tooltip');
-      });
-      $("button").attr('data-tooltip').insertAfter('#send-form');
-   });
