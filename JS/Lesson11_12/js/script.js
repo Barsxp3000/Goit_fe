afdce2cb7c01c5b19d304878a3827868f9dfd5ca
Logunov Vladimir <logunov@live.ruprocon-ten git config --global user.email logunov@live.ru>
2016-10-19 03:06:19 +0300

    Complete HW JS 11-12

--- JS/Lesson11_12/js/script.js
+++ JS/Lesson11_12/js/script.js
@@ -1,39 +1,36 @@
 // Part 1 - Слайдер
 
 $(function() {
-  $('.carousel-hider').slider();
+    $('.carousel-hider').slider();
 });
 
 //Part2 - Шаблонизатор
 
-$(function () {
+$(function() {
 
-var html = $('#page').html();
+    var html = $('#myProfile').html();
 
-var myProfile = [
-  {
-    title: 'Логунов Владимир Сергеевич',
-    content: 'Студент ХНУРЭ и GO_IT'
-  },
-  {
-    title: 'Хочу учить фронтенд, потому что:',
-    reason1: 'Это красиво',
-    reason2: 'Это интересно',
-    reason3: 'Потому что я смогу'
-  },
-  {
-    title: 'Мой контактный телефон',
-    content: '+380505477704'
-  },
-  {
-    title: 'Мой профайл в Facebook',
-    link: 'https://www.facebook.com/barsxp3000'
-  }
-];
+    var myProfile = {
+        myName: 'Логунов Владимир Сергеевич',
+        myPhoto: 'img/img-1.png',
+        myEducation: 'Студент ХНУРЭ и GO_IT',
+        capterFe: 'Хочу учить фронтенд, потому что:',
+        frontend: ['Это красиво','Это интересно', 'Потому что я смогу'],
+        capterPh: 'Мой контактныйтелефон',
+        phone: '+380505477704',
+        capterSk: 'Мой Скайп',
+        skype: 'barsxp3000',
+        capterFb: 'Мой профиль в фейсбуке',
+        facebook: 'http://www.facebook.com/barsxp3000',
+        name: 'Vladimir Logunov',
+        capterFeed: 'Мой фидбек:',
+        myFeedback: 'Ябадабадууу'
 
-var content = tmpl(html, {
-  data: myProfile
-});
+    };
 
-$('.profile').append(content);
+    var content = tmpl(html, {
+        data: myProfile
+    });
+
+    $('#page').append(content);
 });
